a6a1635f3ff2f25024e3391976ef85faea5ba4d6
lselvaraj <CaesarbruAug18!>
2018-09-26 11:29:09 +1000

    apply, call and bind to manipulate this

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-function globalFunction() {
+function globalFunction(welcomeMessage) {
+    console.log(welcomeMessage);
     //Function invoked from the global scope and hence "this" will be the Window object
     console.log(this);
 }
-globalFunction();
+globalFunction("Hello from global invocation!");
 
 const obj = {
     name: 'Lakshmi',
@@ -31,3 +32,12 @@ class Person {
 
 const personObj = new Person('Lakshmi');
 personObj.getWelcomeMessage();
+
+//call, apply and bind - allow us to bind this to an object of choice
+globalFunction.call(obj, 'Hello from "call" where the argument list needs to be a comma separated list!');
+globalFunction.apply(obj, ['Hello from "apply" where the argument list needs to be an array!']);
+
+//Can be used when the same function has to be invoked multiple times bound to the same object
+const boundObjFunction = globalFunction.bind(obj);
+boundObjFunction( 'Hello from bound function using the bind keyword called first time');
+boundObjFunction( 'Hello from bound function using the bind keyword called second time');
